90c1688ac8d0926d8e2f240668e835ee864de397
donatursperma3 <donatursperma3@gmail.com>
2026-08-01 21:14:26 +0700

    fix app force close

--- TMessagesProj/src/main/java/tw/nekomimi/nekogram/helpers/AppRestartHelper.java
+++ TMessagesProj/src/main/java/tw/nekomimi/nekogram/helpers/AppRestartHelper.java
@@ -6,34 +6,18 @@ import static android.content.Intent.FLAG_ACTIVITY_NEW_TASK;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
-import android.os.Bundle;
-import android.os.Process;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-
-public final class AppRestartHelper extends Activity {
-    private static final String KEY_RESTART_INTENTS = "nagramx_restart_intents";
-    private static final String KEY_MAIN_PROCESS_PID = "nagramx_main_process_pid";
+public final class AppRestartHelper {
 
     public static void triggerRebirth(Context context, Intent... nextIntents) {
-        nextIntents[0].addFlags(FLAG_ACTIVITY_NEW_TASK | FLAG_ACTIVITY_CLEAR_TASK);
-        Intent intent = new Intent(context, AppRestartHelper.class);
-        intent.addFlags(FLAG_ACTIVITY_NEW_TASK);
-        intent.putParcelableArrayListExtra(KEY_RESTART_INTENTS, new ArrayList<>(Arrays.asList(nextIntents)));
-        intent.putExtra(KEY_MAIN_PROCESS_PID, Process.myPid());
+        if (context == null || nextIntents == null || nextIntents.length == 0) {
+            return;
+        }
+        Intent intent = nextIntents[0];
+        intent.addFlags(FLAG_ACTIVITY_NEW_TASK | FLAG_ACTIVITY_CLEAR_TASK);
         context.startActivity(intent);
-    }
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        Process.killProcess(getIntent().getIntExtra(KEY_MAIN_PROCESS_PID, -1));
-        ArrayList<Intent> intents = getIntent().getParcelableArrayListExtra(KEY_RESTART_INTENTS);
-        if (intents != null) {
-            startActivities(intents.toArray(new Intent[0]));
-            finish();
-            Runtime.getRuntime().exit(0);
+        if (context instanceof Activity) {
+            ((Activity) context).finishAffinity();
         }
     }
 }
